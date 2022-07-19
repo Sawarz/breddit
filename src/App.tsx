ddf74a745c610db1a8efce62cb2699e3d6e78d79
unknown <sawarz22@gmail.com>
2022-07-19 23:49:09 +0200

    Initial routing and structure of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
+import Navbar from "./components/multiple-use/navbar/Navbar";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import PostCreator from "./components/postCreator/PostCreator";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <HashRouter>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/post" element={<PostCreator />}></Route>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
