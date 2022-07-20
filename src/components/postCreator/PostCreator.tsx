2dfe0144ddd75a2c8bf32119205b3af9f51dafeb
unknown <sawarz22@gmail.com>
2022-07-21 00:03:37 +0200

    Add Community choice

--- src/components/postCreator/PostCreator.tsx
+++ src/components/postCreator/PostCreator.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from './styles.module.css'
+import Dropdown from '../multiple-use/dropdown/Dropdown'
 
 export default function PostCreator() {
+  const [community, setCommunity] = useState("");
+
+  let options = [
+    "Rye",
+    "Wholemeal"
+  ];
+
+  function chooseCommunity(community: string) {
+    setCommunity(community);
+  }
+
   return (
     <div className={styles.postCreator}>
       <div className={styles.title}>Create a post!</div>
-      <div className={styles.chooseCommunity}>Choose a community</div>
+      <Dropdown
+        label={"Choose a community"}
+        options={options}
+        chooseCommunity={chooseCommunity}
+      />
       <div className={styles.postElements}>
         <div className={styles.postElement}>Text</div>
         <div className={styles.postElement}>Image</div>
