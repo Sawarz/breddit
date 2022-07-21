e448a71d94c5e84b82144b6a1d56516894a376ce
Sawarz <sawarz22@gmail.com>
2022-07-21 23:45:34 +0200

    Setup different Post Elements

--- src/components/postCreator/PostCreator.tsx
+++ src/components/postCreator/PostCreator.tsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
+import { ReactComponentElement, useState } from 'react'
 import styles from './styles.module.css'
 import Dropdown from '../multiple-use/dropdown/Dropdown'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileText, faFont, faPaperclip, faSquarePollVertical } from '@fortawesome/free-solid-svg-icons';
+import Text from './tabs/Text';
+import Image from './tabs/Image';
+import Link from './tabs/Link';
+import Poll from './tabs/Poll';
+
+export type Post = {
+  text: undefined | string,
+  image: undefined | File
+}
 
 export default function PostCreator() {
   const [community, setCommunity] = useState("");
+  const [post, setPost] = useState<Post>({
+    text: undefined,
+    image: undefined
+  })
+  const [currentTab, setCurrentTab] = useState(<Text setPost={setPost} post={post}/>)
 
   let options = [
     "Rye",
@@ -12,6 +28,13 @@ export default function PostCreator() {
 
   function chooseCommunity(community: string) {
     setCommunity(community);
+  }
+
+  function renderPostImage(image: Post["image"]){
+    if(image != undefined){
+      return(<img src={URL.createObjectURL(image as File)}></img>)
+    }
+    return(<></>)
   }
 
   return (
@@ -23,11 +46,35 @@ export default function PostCreator() {
         chooseCommunity={chooseCommunity}
       />
       <div className={styles.postElements}>
-        <div className={styles.postElement}>Text</div>
-        <div className={styles.postElement}>Image</div>
-        <div className={styles.postElement}>Link</div>
-        <div className={styles.postElement}>Poll</div>
+        <button className={styles.postElement} onClick={()=>{
+          setCurrentTab(<Text setPost={setPost} post={post}/>);
+        }}>
+          <div className={styles.elementTitle}>Text</div>
+          <FontAwesomeIcon icon={faFont}></FontAwesomeIcon>
+        </button>
+        <button className={styles.postElement} onClick={()=>{
+          setCurrentTab(<Image setPost={setPost} post={post}/>);
+        }}>
+          <div className={styles.elementTitle}>Image</div>
+          <FontAwesomeIcon icon={faFileText}></FontAwesomeIcon>
+        </button>
+        <button className={styles.postElement} onClick={()=>{
+          setCurrentTab(<Link setPost={setPost} post={post}/>);
+        }}>
+          <div className={styles.elementTitle}>Link</div>
+          <FontAwesomeIcon icon={faPaperclip}></FontAwesomeIcon>
+        </button>
+        <button className={styles.postElement} onClick={()=>{
+          setCurrentTab(<Poll setPost={setPost} post={post}/>);
+        }}>
+          <div className={styles.elementTitle}>Poll</div>
+          <FontAwesomeIcon icon={faSquarePollVertical}></FontAwesomeIcon>
+        </button>
       </div>
+      {currentTab}
+      <div>Post preview:</div>
+      <div className={styles.previewText}>{post.text}</div>
+      <div>{renderPostImage(post.image)}</div>
     </div>
   )
 }
