28943a41261eb04ce579c2730d0b92f862434908
unknown <sawarz22@gmail.com>
2022-07-28 22:58:09 +0200

    Add Title to posts

--- src/components/postCreator/tabs/Text.tsx
+++ src/components/postCreator/tabs/Text.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from './styles.module.css'
 import { Post } from '../PostCreator'
 
@@ -7,14 +7,31 @@ type Props = {
     post: Post
 }
 
-export default function Text({setPost, post}: Props) {
+export default function Text({ setPost, post }: Props) {
+  const [text, setText] = useState<string | undefined>(post.text);
+  const [title, setTitle] = useState<string | undefined>(post.title)
+
+  useEffect(() => {
+    setPost({
+      ...post,
+      title: title,
+      text: text
+    })
+  }, [text, title])
+  
   return (
+    <>
+    <textarea className={styles.titleCreator}
+      onChange={(e) => {
+        setTitle(e.target.value);
+        }}
+      >{post.title}</textarea>
     <textarea className={styles.textCreator}
       onChange={(e) => {
-        setPost({
-          ...post,
-            text: e.target.value,
-        })
-    }}>{post.text}</textarea>
+        setText(e.target.value);
+        }}
+      >{post.text}</textarea>
+    </>
+    
   )
 }
