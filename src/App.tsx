7c055940041605355eca456b61f033ef7b8e01d9
unknown <sawarz22@gmail.com>
2022-07-29 21:37:21 +0200

    Add dynamic post routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/multiple-use/navbar/Navbar";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import PostCreator from "./components/postCreator/PostCreator";
+import Post from "./components/post/Post";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/post" element={<PostCreator />}></Route>
+          <Route path="/posts/:postID" element={<Post />}></Route>
         </Routes>
       </div>
     </HashRouter>
