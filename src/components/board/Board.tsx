54f06dd18c05fc571142e1964ba0692116a1462d
unknown <sawarz22@gmail.com>
2022-07-31 17:17:25 +0200

    Changed styling and added likes

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -41,21 +41,23 @@ export default function Board() {
           <div className={styles.profilePic}></div>
           <Link to="/post" className={styles.createPostButton}>Create post</Link>
         </div>
-        <div className={styles.dashboard}>
           <div className={styles.filters}></div>
           <div className={styles.posts}>
             {renderPosts ? posts?.map((post) => {
-              console.log(images);
               if (images != undefined) {
                 let image = images.find(image => image.postID === post.id)
                 if(image != undefined)
-                return (<>
-                  <Link to={`/posts/${post.id}`} className={styles.post}>
-                    <div className={styles.postCommunity}>b/{post.community}</div>
-                    <div className={styles.postTitle}>{post.title}</div>
-                    {image.url !== "noImage" ? <img className={styles.postImage} src={image.url}></img> : null}
-                  </Link>
-                  </>)
+                  return (
+                    <div className={styles.post}>
+                      <div className={styles.postLikes}>{post.likes}</div>
+                      <div className={styles.postContent}>
+                      <Link to={`/posts/${post.id}`} className={styles.link}>
+                        <div className={styles.postCommunity}>b/{post.community}</div>
+                        <div className={styles.postTitle}>{post.title}</div>
+                        {image.url !== "noImage" ? <img className={styles.postImage} src={image.url}></img> : null}
+                      </Link>
+                      </div>
+                    </div>)
               }
               else {
                 return <div>Loading...</div>
@@ -65,7 +67,6 @@ export default function Board() {
               null
             }
           </div>
-        </div>
       </div>
       <div className={styles.addons}></div>
     </div>
