b4db5516c3b769031db68abcf5298a5fd6b3c2f1
Sawarz <sawarz22@gmail.com>
2022-08-03 13:22:53 +0200

    Add styling to loading div

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -64,7 +64,7 @@ export default function Board({loggedIn}: Props) {
                     </div>)
               }
               else {
-                return <div>Loading...</div>
+                return <div className={styles.loading}>Loading...</div>
               }
             })
               :
