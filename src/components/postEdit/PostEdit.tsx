8c9c059547db46b6390bf02a78c35ff512919b33
Sawarz <sawarz22@gmail.com>
2022-08-05 21:29:14 +0200

    Initial postEdit component

---
+++ src/components/postEdit/PostEdit.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import styles from './postedit.module.css'
+import { Post as PostType } from '../postCreator/PostCreator';
+import { useParams } from 'react-router-dom';
+import Firebase from '../../firebase/Firebase';
+
+export default function PostEdit() {
+    const [post, setPost] = useState<PostType>(
+        {
+            id: undefined,
+            title: undefined,
+            text: undefined,
+            community: undefined,
+            user: undefined,
+            comments: undefined
+        }
+    );
+
+    const params = useParams();
+
+    async function getPostData() {
+        if (params.postID !== undefined) {
+            let postFromDB = await Firebase.post.get(params.postID) as PostType;
+            setPost(postFromDB);
+        } 
+    }
+
+    useEffect(() => {
+        getPostData();
+    }, [])
+    
+  return (
+    <div className={styles.postEdit}>
+        <textarea>{post.text}</textarea>
+        <button className={styles.deletePost} onClick={()=>{
+            if(post.id !== undefined)
+                Firebase.post.delete(post.id);
+                window.location.reload();
+        }}>Delete post</button>
+        </div>
+  )
+}
