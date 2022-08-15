e98dbf536e170f56da515a0b197285c0ddd6e3c4
unknown <sawarz22@gmail.com>
2022-08-15 15:14:38 +0200

    Add leave community function

--- src/components/community/Community.tsx
+++ src/components/community/Community.tsx
@@ -15,33 +15,49 @@ type Props = {
 export default function Community({ loggedIn }: Props) {
   const params = useParams();
 
-  const [community, setCommunity] = useState<CommunityType>()
-  const [joined, setJoined] = useState<boolean>(false)
-  const [user, setUser] = useState<User | undefined>()
+  const [community, setCommunity] = useState<CommunityType>();
+  const [joined, setJoined] = useState<boolean>(false);
+  const [user, setUser] = useState<User | undefined>();
 
   async function getCommunityData() {
     if (params.communityID !== undefined) {
       let communityFromDB = await Firebase.community.get(params.communityID) as CommunityType;
-        setCommunity(communityFromDB);
+      setCommunity(communityFromDB);
     } 
   }
 
   async function getUserData() {
-    if (FirebaseCore.auth.currentUser?.uid) {
-      let user = await Firebase.getUser(FirebaseCore.auth.currentUser.uid) as User;
+    let user: User;
+    if (FirebaseCore.auth.currentUser?.uid && community) {
+      user = await Firebase.getUser(FirebaseCore.auth.currentUser.uid) as User;
       setUser(user);
+      if (user.communitiesJoined.find(communityJoined => communityJoined === community.id)) {
+        setJoined(true);
+      }
     }
   }
 
   async function joinCommunity() {
-    if(community && FirebaseCore.auth.currentUser?.uid)
+    if (community && FirebaseCore.auth.currentUser?.uid) {
       Firebase.community.join(community?.id, FirebaseCore.auth.currentUser?.uid)
+      setJoined(true);
+    }
+  }
+
+  async function leaveCommunity() {
+    if (community && FirebaseCore.auth.currentUser?.uid) {
+      Firebase.community.leave(community?.id, FirebaseCore.auth.currentUser?.uid)
+      setJoined(false);
+    }
   }
 
   useEffect(() => {
     getCommunityData();
-    getUserData();
   }, [])
+  
+  useEffect(() => {
+    getUserData();
+  }, [community])
 
   return (
     <div className={styles.community}>
@@ -50,7 +66,11 @@ export default function Community({ loggedIn }: Props) {
         <>
           <div className={styles.communityInfo}>
             <div className={styles.communityName}>{community.name}</div>
+            {joined ?
+            <button className={styles.joinButton} onClick={leaveCommunity}>Leave</button>
+            : 
             <button className={styles.joinButton} onClick={joinCommunity}>Join</button>
+            }
             <div className={styles.communityNumOfMembers}>{community.numOfMembers}</div>
           </div>
           <div className={styles.main}>
