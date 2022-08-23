e2ae8db1b21d645740fe94202be9f2ba28084c10
unknown <sawarz22@gmail.com>
2022-08-23 12:27:58 +0200

    Add username and password change

--- src/components/profile/options/changeData/ChangeData.tsx
+++ src/components/profile/options/changeData/ChangeData.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import FirebaseCore from '../../../../firebase/FirebaseCore'
+import Firebase from '../../../../firebase/Firebase'
+import styles from './changedata.module.css'
+import { updatePassword } from 'firebase/auth'
 
 export default function ChangeData() {
+  const [user, setUser] = useState(FirebaseCore.auth.currentUser?.uid);
+  async function changeNickname() {
+    
+  }
+
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div>ChangeData</div>
+    <div className={styles.option}>
+      <div>Username</div>
+      <input ref={usernameRef}></input>
+      <button onClick={() => {
+        if(user && usernameRef.current?.value)
+          Firebase.user.update(user, "username", usernameRef.current?.value)
+      }}>Confirm</button>
+      <div>Password</div>
+      <input ref={passwordRef}></input>
+      <button onClick={() => {
+        if(user && passwordRef.current?.value && FirebaseCore.auth.currentUser)
+          updatePassword(FirebaseCore.auth.currentUser, passwordRef.current.value).then(() => {
+            // Update successful.
+          }).catch((error) => {
+            // An error ocurred
+            // ...
+          });
+      }}>Confirm</button>
+    </div>
   )
 }
